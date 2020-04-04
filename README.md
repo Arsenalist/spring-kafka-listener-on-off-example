7fedd0cc95d05b3c6d8a084427ea598f63b46995
Zarar Siddiqi <zarars@gmail.com>
2020-04-04 02:58:34 -0400

    first commmit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Starting and Stopping a Kafka Listener after Spring Boot Startup
+
+This example is a fork of the [official example](https://github.com/spring-projects/spring-kafka/tree/master/samples/sample-01).
+
+Start Zookeeper and Kafka from the Kafka directory:
+
+```
+.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
+.\bin\windows\kafka-server-start.bat .\config\server.properties
+```
+
+Two topics need to be present:
+
+1. `topic1` - expects JSON with a key of `foo`, so `{"foo": "bar"}`
+2. `topic1.DLST` - expects a string like "foo"  - is listening on startup
+
+```$xslt
+.\bin\windows\kafka-console-producer.bat --broker-list localhost:9092 --topic topic1
+.\bin\windows\kafka-console-producer.bat --broker-list localhost:9092 --topic topic1.DLT
+```
+
+Run the Spring boot application which starts on `localhost:8080`.
+
+- `topic1` listener has not started as in `@KafkaListener(autoStartup="false")`
+- `topic1.DLT` listener has not started as `@KafkaListener()`'s `autoStartup` value is `"true"` by default
+
+Create a producer from the command line and send a few JSON object to `topic` (which is presently off) which look like:
+
+```
+PS C:\Users\zarar\Tools\kafka_2.13-2.4.1> .\bin\windows\kafka-console-producer.bat --broker-list localhost:9092 --topic topic1                                                                                                                     >{"foo": "bar1"}
+>{"foo": "bar2"}
+>{"foo": "bar3"}
+```
+
+
+Now start the `@KafkaListener` for `topic1` by visiting/CURLing:
+
+http://localhost:8080/start
+
+In the Spring Boot terminal you'll see:
+
+```
+2020-04-04 02:49:54.972  INFO 19072 --- [ fooGroup-0-C-1] com.example.Application                  : Received: Foo2 [foo=bar1]
+2020-04-04 02:49:54.972  INFO 19072 --- [ fooGroup-0-C-1] com.example.Application                  : Received: Foo2 [foo=bar2]
+2020-04-04 02:49:54.972  INFO 19072 --- [ fooGroup-0-C-1] com.example.Application                  : Received: Foo2 [foo=bar3]
+```
+
+To stop listening:
+
+http://localhost:8080/stop
---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
+import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.util.backoff.FixedBackOff;
+
+import com.common.Foo2;
+
+/**
+ * Sample shows use of a dead letter topic.
+ *
+ * @author Gary Russell
+ * @since 2.2.1
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	private final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	private final TaskExecutor exec = new SimpleAsyncTaskExecutor();
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args).close();
+	}
+
+	/*
+	 * Boot will autowire this into the container factory.
+	 */
+	@Bean
+	public SeekToCurrentErrorHandler errorHandler(KafkaTemplate<Object, Object> template) {
+		return new SeekToCurrentErrorHandler(
+				new DeadLetterPublishingRecoverer(template), new FixedBackOff(1000L, 2));
+	}
+
+	@Bean
+	public RecordMessageConverter converter() {
+		return new StringJsonMessageConverter();
+	}
+
+	@KafkaListener(id = "fooGroup", topics = "topic1", autoStartup = "false")
+	public void listen(Foo2 foo) {
+		logger.info("Received: " + foo);
+		if (foo.getFoo().startsWith("fail")) {
+			throw new RuntimeException("failed");
+		}
+		this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
+	}
+
+	@KafkaListener(id = "dltGroup", topics = "topic1.DLT")
+	public void dltListen(String in) {
+		logger.info("Received from DLT: " + in);
+		this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return new NewTopic("topic1", 1, (short) 1);
+	}
+
+	@Bean
+	public NewTopic dlt() {
+		return new NewTopic("topic1.DLT", 1, (short) 1);
+	}
+
+	@Bean
+	public ApplicationRunner runner() {
+		return args -> {
+			System.out.println("Hit Enter to terminate...");
+			System.in.read();
+		};
+	}
+
+}
